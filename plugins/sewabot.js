425756549365de4044627792e054ea59e384cba3
SaboBOTZMD <103872974+SaboBOTZMD@users.noreply.github.com>
2022-04-19 20:39:16 +0800

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -22,7 +22,7 @@ let handler = async(m, { conn }) => {
         sections:  [{title: "Silahkan di pilih", rows: [
         {title: 'PERMINGGU', description: "Rp.3.000\nSewa bot PERMINGGU.", rowId:".masuk"},
         {title: 'PERBULAN', description: "Rp.5.000\nSewa bot PERBULAN.", rowId:".masuk"},
-        {title: 'PERMANENT', description: "Rp.10.000\nSewa bot PERMANENT.", rowId:".masuk"},
+        {title: 'PERMANENT', description: "Rp.7.000\nSewa bot PERMANENT.", rowId:".masuk"},
         {title: 'Owner', description: "Chat owner nya jika ada perlu.", rowId:".owner"},
         {title: 'Rules', description: "Rules Bot Sabo.", rowId:".snk"},
        ] }],
