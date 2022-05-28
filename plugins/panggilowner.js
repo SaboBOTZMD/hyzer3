8e972d4c6fa05c15f7aca9e5dd48a0915133769e
Sabo Ofc <103872974+SaboBOTZMD@users.noreply.github.com>
2022-05-28 09:29:28 +0800

    Update panggilowner.js

--- plugins/panggilowner.js
+++ plugins/panggilowner.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m) => {
 let stc = fs.readFileSync('./src/tag.webp')
-conn.fakeReply(m.chat, stc, '6282145203493@s.whatsapp.net', '*Ngapain Tag Owner Gw Deck, Mau Nyewa Bot?*')
+conn.fakeReply(m.chat, stc, '628815952469@s.whatsapp.net', '*Ngapain Tag Owner Gw Deck, Mau Nyewa Bot?*')
 }
 handler.customPrefix = /^(@6282145203493)$/i
 handler.command = new RegExp
