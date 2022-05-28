4e74ba755a6bd1379685a827cb07b853ff8d0e21
Sabo Ofc <103872974+SaboBOTZMD@users.noreply.github.com>
2022-05-28 09:28:52 +0800

    Update owneryve.js

--- plugins/owneryve.js
+++ plugins/owneryve.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-	this.sendContact(m.chat, '60189830350', 'Owner Bot Lyn :)', m)
+	this.sendContact(m.chat, '628815952469', 'Owner Sabo Botz :)', m)
 }
 
 handler.customPrefix = ['Owner bot'] 
